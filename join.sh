67e1174fc508487aace12de6359acf12a221dbaa
lucky-grace <zhe.tao1@students.mq.edu.au>
2021-04-20 09:52:10 +1000

    add tab

--- join.sh
+++ join.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-join --nocheck-order  -o 2.1 2.3 1.2 2.2 expression.tab annotation.tab  | tee output.tab
+join -t$'\t' --nocheck-order  -o 2.1 2.3 1.2 2.2 expression.tab annotation.tab  | tee output.tab
